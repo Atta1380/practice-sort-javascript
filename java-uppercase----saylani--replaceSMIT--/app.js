cfdaf708ffc1ea7017e917a988c1e933e7eaea08
Muhammad Atta ul Mustafa <120771673+Atta1380@users.noreply.github.com>
2023-02-28 20:58:26 +0500

    added html and javascript files

---
+++ java-uppercase----saylani--replaceSMIT--/app.js
@@ -0,0 +1,26 @@
+//----all words in uppercase----------
+// var str = "ghous ahmed saylani"
+// var arr = str.split(" ")
+// for (var i =0;i<arr.length; i++){
+//     arr[i]=arr[i][0].toUpperCase()+arr[i].slice(i)
+// }
+// str = arr.join(" ")
+// console.log(str)
+
+
+
+//----------- in paragraph saylani replace SMIT----
+
+// var paraghraph="ghous ahmed saylani raza saylani"
+// var findto ="saylani"
+// var replaceto="SMIT"
+// for(var i=0;i<paraghraph.length; i++){
+//     if(paraghraph.slice(i,i+findto.length)==findto){
+//         paraghraph=paraghraph.slice(0,i)+replaceto+paraghraph.slice(i+findto.length)
+//     }
+
+// }
+
+// console.log(paraghraph)
+
+//-----
